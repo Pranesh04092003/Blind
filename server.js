fbfb471f59c5b74647a6d33226c12c472b5845f4
Pranesh RV <139009385+Pranesh04092003@users.noreply.github.com>
2025-01-08 18:33:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blind')
+mongoose.connect(process.env.MONGODB_URI )
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
