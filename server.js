4c5451c6a7ceaac114f6679457ddb3328b2e3d7f
Pranesh RV <139009385+Pranesh04092003@users.noreply.github.com>
2025-01-08 18:33:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blind')
 
 // Define Schema
 const dataSchema = new mongoose.Schema({
-    title: {
+    character: {
         type: String,
         required: true
     }
